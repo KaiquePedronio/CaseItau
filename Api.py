a902b8540293665faa8dec1d1984a9d8a89bd5fa
KaiquePedronio <69361206+KaiquePedronio@users.noreply.github.com>
2020-08-08 20:44:37 -0300

    Update Api.py

--- Api.py
+++ Api.py
@@ -75,4 +75,4 @@ if __name__ == '__main__':
     conn, cursor = Dbase(pathresidenci_git, pathmediapreco_git).Main()
 
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="127.0.0.1", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=True)
